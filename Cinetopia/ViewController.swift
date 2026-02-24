5588642d4bc7f745a4eda600d349fbd0d72d22b6
Douglas Silva <douglasferreirasilva.si@gmail.com>
2026-02-24 10:54:09 -0300

    Initial Commit

---
+++ Cinetopia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cinetopia
+//
+//  Created by Douglas Silva on 24/02/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
